b5be9046e395bd7865fdf1330e3d000e8f8a6b94
rainyhhhh <yuqingh@student.unimelb.edu.au>
2014-10-13 04:20:40 +1100

    private browsing update

---
+++ relationshipproject/app/views/removeHistory.blade.php
@@ -0,0 +1,235 @@
+@extends('layouts.main')
+
+@section('title')
+<title>Isha SoulAce - Removing History</title>
+@stop
+
+@section('main')
+<style type="text/css">
+.auth .auth-box{
+  background-color: white;
+  max-width:660px;
+  margin:0 auto;
+  border:1px solid rgba(255, 255, 255, 0.4);;
+  margin-top:40px;
+  -webkit-box-shadow: 0px 0px 30px 0px rgba(50, 50, 50, 0.32);
+  -moz-box-shadow:    0px 0px 30px 0px rgba(50, 50, 50, 0.32);
+  box-shadow:         0px 0px 30px 0px rgba(50, 50, 50, 0.32);
+  -webkit-border-radius: 3px;
+  -moz-border-radius: 3px;
+  border-radius: 3px;
+}
+.clickable
+{
+  cursor: pointer;
+}
+
+.clickable .glyphicon
+{
+  background: rgba(0, 0, 0, 0.15);
+  display: inline-block;
+  padding: 6px 12px;
+  border-radius: 4px
+}
+
+.panel-heading span
+{
+  margin-top: -23px;
+  font-size: 15px;
+  margin-right: -9px;
+}
+a.clickable { color: inherit; }
+a.clickable:hover { text-decoration:none; }
+</style>
+<div class="page-header">
+  <h3 class="text-center">Private Browsing</h1>
+</div>
+<div class="container auth">
+  <div id="big-form" class="well auth-box">
+    <!-- Form Name -->
+    <legend class="text-center" style="border-bottom:solid 1px;">For Google Chrome <!--(latest version)--></legend>
+    <div class="col-md-12">
+      <div class="panel panel-danger">
+        <div class="panel-heading">
+          <h3 class="panel-title">
+            Clear your entire browsing history
+          </h3>
+          <span class="pull-right clickable"><i class="glyphicon glyphicon-minus"></i></span>
+        </div>
+        <div class="panel-body">
+          <p>1. Click the Chrome menu <img src="http://storage.googleapis.com/support-kms-prod/SNP_9721C573E41777FBA43E1498AD084E3B160C_2696434_en_v2" width="29" height="29" alt="Chrome menu" title="Chrome menu"> on the browser toolbar.</p>
+          <p>2. Select <STRONG>Tools</STRONG>.</p>
+          <p>3. Select <STRONG>Clear Browsing Data</STRONG>.</p>
+          <p>4. In the dialog that appears, select the "Clear browsing history" checkbox.</p>
+          <p>5. Use the menu at the top to select the amount of data you want to delete. Select <STRONG>beginning of time</STRONG> to clear your entire browsing history.</p>
+          <p>2. Click <STRONG>Clear browsing data</STRONG>.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-12">
+      <div class="panel panel-danger">
+        <div class="panel-heading">
+          <h3 class="panel-title">
+            Clear specific items from your browsing history
+          </h3>
+          <span class="pull-right clickable"><i class="glyphicon glyphicon-minus"></i></span>
+        </div>
+        <div class="panel-body">
+          <p>1. Click the Chrome menu <img src="http://storage.googleapis.com/support-kms-prod/SNP_9721C573E41777FBA43E1498AD084E3B160C_2696434_en_v2" width="29" height="29" alt="Chrome menu" title="Chrome menu"> on the browser toolbar.</p>
+          <p>2. Select <STRONG>History</STRONG>.</p>
+          <p>3. Mouse over the items you'd like to remove.</p>
+          <p>4. Select the checkboxes for the items.</p>
+          <p>5. Click the <STRONG>Remove selected items</STRONG> button at the top of the page.</p>
+          <p>2. Click <STRONG>OK</STRONG> when the confirmation window appears.</p>
+        </div>
+      </div>
+      <p>More Details, <a href="https://support.google.com/chrome/answer/95537?hl=en" target="_blank">Click here</a></p>
+      <p>To be easier, <a href="https://chrome.google.com/webstore/detail/history-eraser/gjieilkfnnjoihjjonajndjldjoagffm" target="_blank">History Eraser</a> is a useful plugin to help you.</p> 
+      <p>You can also use the keyboard shortcuts <strong>Ctrl+Shift+N</strong> (Windows, Linux, and Chrome OS) and <strong>⌘-Shift-N</strong> (Mac) to open an incognito window.</p>
+    </div>       
+    <legend class="text-center" style="border-bottom:solid 1px;">For Firefox 
+    </legend>
+    <div class="col-md-12">
+      <div class="panel panel-danger">
+        <div class="panel-heading">
+          <h3 class="panel-title">
+            Clear your entire browsing history
+          </h3>
+          <span class="pull-right clickable"><i class="glyphicon glyphicon-minus"></i></span>
+        </div>
+        <div class="panel-body">
+          <p>1. Click the menu button <img alt="New Fx Menu" class="wiki-image frameless" data-original-src="http://support.cdn.mozilla.net/media/uploads/gallery/images/2014-01-10-13-08-08-f52b8c.png" src="http://support.cdn.mozilla.net/media/uploads/gallery/images/2014-01-10-13-08-08-f52b8c.png" title="">
+            , choose <STRONG>History</STRONG>, and then <span>Clear Recent History...</span>
+          </p>
+          <p>2. Select how much history you want to clear:</p>
+          <ul>
+            <li> Click the drop-down menu next to <strong>Time range to clear</strong> to choose how much of your history Firefox will clear.</li>
+            <dl>
+              <dd>
+                <span class="for" data-for="win" style="display: none;">  
+                  <img alt="History Win2" class="wiki-image frameless lazy" data-original-src="//support.cdn.mozilla.net/media/uploads/gallery/images/2011-02-03-06-23-39-b1c923.png" src="//support.cdn.mozilla.net/static/img/placeholder.gif" title="">
+                </span>
+                <span class="for" data-for="mac">  
+                  <img alt="History Mac2" class="wiki-image frameless" data-original-src="//support.cdn.mozilla.net/media/uploads/gallery/images/2011-02-03-06-21-47-3e04af.png" src="//support.cdn.mozilla.net/media/uploads/gallery/images/2011-02-03-06-21-47-3e04af.png" title="">
+                </span>
+                <span class="for" data-for="linux" style="display: none;">  
+                  <img alt="History Lin2" class="wiki-image frameless lazy" data-original-src="//support.cdn.mozilla.net/media/uploads/gallery/images/2011-02-03-06-19-46-d382ba.png" src="//support.cdn.mozilla.net/static/img/placeholder.gif" title="">
+                </span>
+              </dd>
+            </dl>
+            <li>Next, click the arrow next to <strong>Details</strong> to select exactly what information will get cleared. Your choices are described in the <a href="#w_what-things-are-included-in-my-history">What things are included in my history?</a> section above.</li>
+            <dl>
+              <dd>
+                <span class="for" data-for="win" style="display: none;">  
+                  <img alt="History Win3 Fx11" class="wiki-image frameless lazy" data-original-src="//support.cdn.mozilla.net/media/uploads/gallery/images/2012-02-17-14-54-33-834a5e.png" src="//support.cdn.mozilla.net/static/img/placeholder.gif" title="">
+                </span>
+                <span class="for" data-for="mac">  
+                  <img alt="History Mac3 Fx11" class="wiki-image frameless" data-original-src="//support.cdn.mozilla.net/media/uploads/gallery/images/2012-02-17-15-07-30-5a7b45.png" src="//support.cdn.mozilla.net/media/uploads/gallery/images/2012-02-17-15-07-30-5a7b45.png" title="">
+                </span>
+                <span class="for" data-for="linux" style="display: none;">  
+                  <img alt="History Lin3 Fx11" class="wiki-image frameless lazy" data-original-src="//support.cdn.mozilla.net/media/uploads/gallery/images/2012-02-17-15-07-07-228bae.png" src="//support.cdn.mozilla.net/static/img/placeholder.gif" title="">
+                </span>
+              </dd>
+            </dl>
+          </ul>
+          <p>3. Finally, click the <STRONG>Clear Now</STRONG> button. The window will close and the items you've selected will be cleared.</p>
+        </div>
+      </div>
+      <p>More Details, <a href="https://support.mozilla.org/en-US/kb/remove-recent-browsing-search-and-download-history#w_how-do-i-clear-my-history" target="_blank">Click here</a></p>
+      <span>To open a new private window, click the menu button   
+<img alt="New Fx Menu" class="wiki-image frameless" data-original-src="//support.cdn.mozilla.net/media/uploads/gallery/images/2014-01-10-13-08-08-f52b8c.png" src="//support.cdn.mozilla.net/media/uploads/gallery/images/2014-01-10-13-08-08-f52b8c.png" title="">
+
+ and then click <span class="button">New Private Window</span>. </span>
+
+    </div>
+    <legend class="text-center" style="border-bottom:solid 1px;">For Internet Explorer (IE 9)</legend>
+    <div class="col-md-12">
+      <div class="panel panel-danger">
+        <div class="panel-heading">
+          <h3 class="panel-title">
+            Delete all or some of your browsing history
+          </h3>
+          <span class="pull-right clickable"><i class="glyphicon glyphicon-minus"></i></span>
+        </div>
+        <div class="panel-body">
+          <p>1. Open Internet Explorer by clicking the <STRONG>Start</STRONG> button <img src="http://res2.windows.microsoft.com/resbox/en/windows%207/main/4f6cbd09-148c-4dd8-b1f2-48f232a2fd33_818.jpg">. In the search box, type <STRONG>Internet Explorer</STRONG>, and then, in the list of results, click <STRONG>Internet Explorer</STRONG>.</p>
+          <p>2. Click the <STRONG>Tools</STRONG> button <img class="embedObject" src="http://res2.windows.microsoft.com/resbox/en/windows 7/main/448ef968-d58a-43a2-b0cc-f396f4024f2c_14.jpg" alt="Tools button" data-id="pageContainer0_ID0EAE">, point to <STRONG>Safety</STRONG>, and then click <STRONG>Delete browsing history</STRONG>. If you don't want to delete the cookies and files associated with websites in your favorites list, select the <STRONG>Preserve Favorites website data</STRONG> check box.</p>
+          <p>3. Select the check box next to each category of information you want to delete.</p>
+          <p>4. Click <STRONG>Delete</STRONG>.</p>
+        </div>
+      </div>
+      <span>More Details, <a href="http://windows.microsoft.com/en-us/windows7/how-to-delete-your-browsing-history-in-internet-explorer-9" target="_blank">Click here</a></span>
+    </div>
+    <!--IE-->
+    <legend class="text-center" style="border-bottom:solid 1px;">For Safari 6</legend>
+    <div class="col-md-12">
+      <div class="panel panel-danger">
+        <div class="panel-heading">
+          <h3 class="panel-title">
+            Clear the entire history list
+          </h3>
+          <span class="pull-right clickable"><i class="glyphicon glyphicon-minus"></i></span>
+        </div>
+        <div class="panel-body">
+          <p>1. Open Safari if it is not already open.</p>
+          <p>2. Choose History > Clear History, and then click Clear.</p>
+          <p>To clear Top Sites as well as your history list, select “Also reset Top Sites.” The Top Sites page is based on your browsing history.</p>
+          <p>Clearing your history also empties caches, removes website icons, and clears the list of recent searches. Website icons are small graphics that help identify sites on the Internet. You see them in the address and search field, the bookmarks list, and other places. You can see your recent searches by clicking the magnifying glass at the left end of the address and search field when it’s empty.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-12">
+      <div class="panel panel-danger">
+        <div class="panel-heading">
+          <h3 class="panel-title">
+            Clear individual items from the history list
+          </h3>
+          <span class="pull-right clickable"><i class="glyphicon glyphicon-minus"></i></span>
+        </div>
+        <div class="panel-body">
+          <p>1. Open Safari if it is not already open.</p>
+          <p>2. Click the open-book icon in the bookmarks bar.<img src="http://support.apple.com/library/APPLE/APPLECARE_ALLGEOS/Product_Help/en_US/PUBLIC_USERS/PP427/Bookmarks.png" alt="Bookmark list button" data-hires="true" data-hires-status="considered"></p>
+          <p>3. Click History in the Collections list on the left.</p>
+          <p>4. Select the items you want to clear in the list of bookmarks on the right, and then press the Delete key.
+If you can’t see the list of bookmarks on the right because Cover Flow view is covering it, drag up on the handle below the Cover Flow scroll bar.</p>
+        </div>
+      </div>
+      <span>More Details, <a href="http://support.apple.com/kb/ph11911" target="_blank">Click here</a></span>
+    </div>
+    <legend></legend>
+    
+  </div>
+  <div class="clearfix"></div>
+</div>
+{{ HTML::script('js/plugins/dataTables/jquery.js') }}
+{{ HTML::script('js/bootstrap.min.js') }}
+<script type="text/javascript">
+$(document).on('click', '.panel-heading span.clickable', function (e) {
+  var $this = $(this);
+  if (!$this.hasClass('panel-collapsed')) {
+    $this.parents('.panel').find('.panel-body').slideUp();
+    $this.addClass('panel-collapsed');
+    $this.find('i').removeClass('glyphicon-minus').addClass('glyphicon-plus');
+  } else {
+    $this.parents('.panel').find('.panel-body').slideDown();
+    $this.removeClass('panel-collapsed');
+    $this.find('i').removeClass('glyphicon-plus').addClass('glyphicon-minus');
+  }
+});
+$(document).on('click', '.panel div.clickable', function (e) {
+  var $this = $(this);
+  if (!$this.hasClass('panel-collapsed')) {
+    $this.parents('.panel').find('.panel-body').slideUp();
+    $this.addClass('panel-collapsed');
+    $this.find('i').removeClass('glyphicon-minus').addClass('glyphicon-plus');
+  } else {
+    $this.parents('.panel').find('.panel-body').slideDown();
+    $this.removeClass('panel-collapsed');
+    $this.find('i').removeClass('glyphicon-plus').addClass('glyphicon-minus');
+  }
+});
+$(document).ready(function () {
+  $('.panel-heading span.clickable').click();
+  $('.panel div.clickable').click();
+});
+</script>

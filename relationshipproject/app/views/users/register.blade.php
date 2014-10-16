bdb549aff61a794b2f1831ecd50d515ac5311b40
rainyhhhh <yuqingh@student.unimelb.edu.au>
2014-10-16 11:28:22 +1100

    remove bootstrap.js in register

--- relationshipproject/app/views/users/register.blade.php
+++ relationshipproject/app/views/users/register.blade.php
@@ -106,9 +106,6 @@
 <script type="text/javascript"
         src="http://cdnjs.cloudflare.com/ajax/libs/jquery/1.8.3/jquery.min.js">
 </script> 
-<script type="text/javascript"
-        src="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.2.2/js/bootstrap.min.js">
-</script>
 
 <script type="text/javascript">
     function change() {

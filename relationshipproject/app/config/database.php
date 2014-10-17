9ac00048f3a1004281346dc3cd92c61665484f1b
rainyhhhh <yuqingh@student.unimelb.edu.au>
2014-10-17 15:13:54 +1100

    update sponsor email and donation page

--- relationshipproject/app/config/database.php
+++ relationshipproject/app/config/database.php
@@ -55,9 +55,9 @@ return array(
 'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'relationship',
-			'username'  => 'root',
-			'password'  => 'root',
+            'database'  => 'relationship2',
+            'username'  => 'root',
+            'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
--- relationshipproject/app/views/emails/sponsor.blade.php
+++ relationshipproject/app/views/emails/sponsor.blade.php
@@ -1,14 +1,47 @@
-{{ Form::open(array('url'=>'emails/send', 'class'=>'form-email')) }}
-	<h2 class="email-heading">Email</h2>
+{{ HTML::style('css/plugins/bootstrap.css') }}
+{{ HTML::style('css/bootstrap-responsive.css') }}
+<style>
+.alert-message
+{
+    margin: 20px 0;
+    padding: 20px;
+    border-left: 3px solid #eee;
+}
+.alert-message h4
+{
+    margin-top: 0;
+    margin-bottom: 5px;
+}
+.alert-message p:last-child
+{
+    margin-bottom: 0;
+}
+.alert-message code
+{
+    background-color: #fff;
+    border-radius: 3px;
+}
+.alert-message-danger
+{
+    background-color: #fdf7f7;
+    border-color: #d9534f;
+}
+.alert-message-danger h4
+{
+    color: #d9534f;
+}
+</style>
 
-	<ul>
-		@foreach($errors->all() as $error)
-			<li>{{ $error }}</li>
-		@endforeach
-	</ul>
+<div class="row">
+               <div class="col-sm-8 col-md-8">
+            <div class="alert-message alert-message-danger">
+                <h4>{{$name}}</h4>
+                <p>Thank you for your support!</p>
+                <p>We will try to contact you on {{$contact_date}}.</p>
+                <p style="margin-left:10px;">Regards,</p>
+                <p><STRONG>Isha SoulAce</STRONG>
+            </div>
+        </div>
+    </div>
 
-    {{ Form::text('text', null, array('class'=>'input-block-level', 'placeholder'=>'Email Message')) }}
-	{{ Form::text('email', null, array('class'=>'input-block-level', 'placeholder'=>'Email Address')) }}
 
-	{{ Form::submit('Send', array('class'=>'btn btn-large btn-primary')) }}
-{{ Form::close() }}
